810dec2c2678dcb50189620d8e9f33f6a0c8d4ae
Bihanskiy <bohdan.bihanskiy@gmail.com>
2022-06-29 16:53:39 +0300

    Deployed website new wrld

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -108,7 +108,7 @@ const View = ({char}) => {
             <div className="char__descr">
                 {description}
             </div>
-            <div className="char__comics">Comics:</div>
+            <div className="char__comics">Comics there:</div>
             <ul className="char__comics-list">
                 {comics.length > 0 ? null : 'There is no comics with this character'}
                 {
